5ac89232a30957019db29470b41be962b9abb60f
stephane mdz <stephanemodzinu@gmail.com>
2024-10-17 15:38:36 +0200

    insertion index.php et config.php

---
+++ index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./public/css/main.css">
+    <title>TOP 10 LANGAGE</title>
+</head>
+<body>
+    <header>
+        <h1>
+            Classement top 10 langage
+        </h1>
+    </header>
+    <main >
+        <div  class="table-primary" role="region" aria-labelledby="compilaton"> 
+            <table id="compilaton">
+                <caption>
+                    Classement des langages
+                </caption>
+                <thead>
+                    <tr>
+                        <th>item 1</th>
+                        <th>item 2</th>
+                        <th>item 3</th>
+                        <th>item 4</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                    </tr>
+                    <tr>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                    </tr>
+                    <tr>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                    </tr>
+                    <tr>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                    </tr>
+                    <tr>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                    </tr>
+                    <tr>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                    </tr>
+                    <tr>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                    </tr>
+                    <tr>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                    </tr>
+                    <tr>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                    </tr>
+                    <tr>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                        <td>content 1</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
+    <footer>
+        
+    </footer>
+</body>
+</html>
